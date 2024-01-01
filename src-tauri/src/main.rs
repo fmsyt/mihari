7ea3e77cdabbfd521e97638d10049d7bd13a963b
motsuni <motsuni@yotta-rc.dev>
2024-01-01 20:15:52 +0900

    system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,32 @@
 
 mod commands;
 
+use tauri::{SystemTray, CustomMenuItem, SystemTrayMenu, SystemTrayEvent, AppHandle};
 use commands::{
     cpu_state,
     memory_state,
     swap_state,
 };
+
+fn create_task_tray() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+
+    let tray = SystemTrayMenu::new()
+        .add_item(quit);
+
+    SystemTray::new().with_menu(tray)
+}
+
+fn handle_system_tray(app: &AppHandle, event: SystemTrayEvent) {
+    match event {
+        SystemTrayEvent::MenuItemClick { id, .. } => {
+            if id == "quit" {
+                app.exit(0)
+            }
+        }
+        _ => {}
+    }
+}
 
 fn main() {
     tauri::Builder::default()
@@ -16,6 +37,8 @@ fn main() {
             memory_state,
             swap_state,
         ])
+        .system_tray(create_task_tray())
+        .on_system_tray_event(handle_system_tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
