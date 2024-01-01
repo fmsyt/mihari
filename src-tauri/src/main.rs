81dbbdb60e11e498225275c26cc859e769578b43
motsuni <motsuni@yotta-rc.dev>
2024-01-01 23:35:44 +0900

    focus on click systemtray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,24 +3,38 @@
 
 mod commands;
 
-use tauri::{SystemTray, CustomMenuItem, SystemTrayMenu, SystemTrayEvent, AppHandle};
-use commands::{
-    cpu_state,
-    memory_state,
-    swap_state,
+use commands::{cpu_state, memory_state, swap_state};
+use tauri::{
+    AppHandle, CustomMenuItem, GlobalWindowEvent, Manager, SystemTray, SystemTrayEvent,
+    SystemTrayMenu, WindowEvent,
 };
+
+fn handle_event(event: GlobalWindowEvent) {
+    match event.event() {
+        WindowEvent::CloseRequested { api, .. } => {
+            api.prevent_close();
+            event.window().hide().unwrap();
+        }
+        _ => {}
+    }
+}
 
 fn create_task_tray() -> SystemTray {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
 
-    let tray = SystemTrayMenu::new()
-        .add_item(quit);
+    let tray = SystemTrayMenu::new().add_item(quit);
 
     SystemTray::new().with_menu(tray)
 }
 
 fn handle_system_tray(app: &AppHandle, event: SystemTrayEvent) {
     match event {
+        SystemTrayEvent::LeftClick { .. } => {
+            let window = app.get_window("main").unwrap();
+
+            window.show().unwrap();
+            window.set_focus().unwrap();
+        }
         SystemTrayEvent::MenuItemClick { id, .. } => {
             if id == "quit" {
                 app.exit(0)
@@ -37,6 +51,7 @@ fn main() {
             memory_state,
             swap_state,
         ])
+        .on_window_event(handle_event)
         .system_tray(create_task_tray())
         .on_system_tray_event(handle_system_tray)
         .run(tauri::generate_context!())
