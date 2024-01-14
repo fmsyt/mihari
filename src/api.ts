b963b9e0ecae02121a4a5de641c2f3eaecac2c5a
motsuni <motsuni@yotta-rc.dev>
2024-01-15 00:24:31 +0900

    calc average on js

--- src/api.ts
+++ src/api.ts
@@ -6,20 +6,39 @@ export async function getAppConfig(): Promise<AppConfig> {
   return await invoke("get_app_config");
 }
 
-export async function getCpuState(ms?: number): Promise<CPUState[]> {
-  return await invoke("cpu_state", { ms });
+export async function getCpuState(): Promise<CPUState[]> {
+  return await invoke("cpu_state");
 }
 
-export async function getCpuCoreState(
-  core: number,
-  ms?: number,
-): Promise<CPUState> {
-  const cores = await getCpuState(ms);
+export async function getCpuCoreState(core: number): Promise<CPUState> {
+  const cores = await getCpuState();
   return cores[core];
 }
 
-export async function getCpuStateAggregate(ms?: number): Promise<CPUState> {
-  return await invoke("cpu_state_aggregate", { ms });
+export async function getCpuStateAggregate(): Promise<CPUState> {
+  const cores = await getCpuState();
+  const averaged = cores.reduce(
+    (prev, cur) => {
+      prev.user += cur.user;
+      prev.nice += cur.nice;
+      prev.system += cur.system;
+      prev.idle += cur.idle;
+      prev.interrupt += cur.interrupt;
+
+      return prev;
+    },
+    { user: 0, nice: 0, system: 0, idle: 0, interrupt: 0 } as CPUState
+  );
+
+  const aggregate = {
+    user: averaged.user / cores.length,
+    nice: averaged.nice / cores.length,
+    system: averaged.system / cores.length,
+    idle: averaged.idle / cores.length,
+    interrupt: averaged.interrupt / cores.length,
+  };
+
+  return aggregate;
 }
 
 export async function getMemoryState(): Promise<MemoryState> {
